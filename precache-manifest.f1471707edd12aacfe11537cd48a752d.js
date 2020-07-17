50f6b58be2c91a2f0df201193e94139984536e85
MAYANKshahh <shahmayank.shah48@gmail.com>
2020-07-16 20:47:16 -0400

    Updates

---
+++ precache-manifest.f1471707edd12aacfe11537cd48a752d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68277ccabad7ec354da07b9625b15e72",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "9a09f375182905460460",
+    "url": "/Todo/static/css/main.b30d8964.chunk.css"
+  },
+  {
+    "revision": "05838e9c262d4f616fa3",
+    "url": "/Todo/static/js/2.50869c2e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Todo/static/js/2.50869c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a09f375182905460460",
+    "url": "/Todo/static/js/main.6d9d9c84.chunk.js"
+  },
+  {
+    "revision": "13754fdb9ad14a151874",
+    "url": "/Todo/static/js/runtime-main.5f11bbab.js"
+  }
+]);
